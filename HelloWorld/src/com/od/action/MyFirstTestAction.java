4a437f645c11ecfa8d467b3ebbb723d236b446d4
huawei-zhu <zhw_019@163.com>
2014-11-24 15:05:31 +0800

    Update MyFirstTestAction.java

--- HelloWorld/src/com/od/action/MyFirstTestAction.java
+++ HelloWorld/src/com/od/action/MyFirstTestAction.java
@@ -2,6 +2,6 @@ package com.od.action;
 
 public class MyFirstTestAction {
 	public static void main(String[] args) {
-		System.out.println("mushroomABCD");
+		System.out.println("mushroomABB");
 	}
 }
