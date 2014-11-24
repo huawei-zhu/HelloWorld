2ca806379a7a7f7ae96bed0afa82a874f99e2f82
huawei-zhu <huawei-zhu@PB1ZGB5.na.odcorp.net>
2014-11-24 12:43:16 +0800

    aaa

--- HelloWorld/src/com/od/action/MyFirstTestAction.java
+++ HelloWorld/src/com/od/action/MyFirstTestAction.java
@@ -3,11 +3,8 @@ package com.od.action;
 public class MyFirstTestAction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-
-
-		System.out.println("hello world##!!");
+		System.out.println("mushroom");
 
 	}
 
