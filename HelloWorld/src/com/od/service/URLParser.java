9886a0a1849979f66dc5eb477759b1179101aa92
huawei-zhu <huawei-zhu@PB1ZGB5.na.odcorp.net>
2015-05-15 16:14:03 +0800

    test

--- HelloWorld/src/com/od/service/URLParser.java
+++ HelloWorld/src/com/od/service/URLParser.java
@@ -7,8 +7,10 @@ public class URLParser {
 	public static void main(String[] args) throws Exception {
 
 		String str = "https://localhost:80/account/myfiles.do?query=abc";
+		String str1 = "hello";
 		URL url = new URL(str);
 
+		System.out.println("path:" + url.getPath());
 		System.out.println("path:" + url.getPath());
 		System.out.println("query:" + url.getQuery());
 		System.out.println("file:" + url.getFile());
